170a4706cf93656e188c73bba4b66fd8e39c4ccb
itastylt <100284073+itastylt@users.noreply.github.com>
2022-12-18 17:16:00 +0200

    fix

--- control/userController.js
+++ control/userController.js
@@ -8,6 +8,33 @@ let getRegisterPage = (req, res ) => {
     return res.render("register.ejs");
 };
 
+let loginUser = (req, res) => {
+    try {
+        let session = req.session;
+        let name = req.body.name;
+        let password = req.body.password;
+
+        sql.query(`SELECT * FROM Naudotojai WHERE Vardas='${name}' AND Slaptazodis='${password}'`, (err, result) => {
+            if(err) {
+                return console.log(err);
+            }
+
+            if(result.length > 0) {
+                //session.id = result.id;
+                console.log(result[0].id_Naudotojas);
+                return res.redirect('/');
+            }
+
+
+        });
+    } catch {
+        console.log("error");
+    }
+}
+let getProfilePage = (req,res) => {
+    console.log(req.params);
+    res.render("profile.ejs");
+}
 let registerNewUser = (req, res) => {
     try {
         let newUser = {
@@ -25,15 +52,18 @@ let registerNewUser = (req, res) => {
             country: req.body.country
         }
         
-        sql.query(`INSERT INTO Naudotojai(Vardas, Elektroninis_pastas, Slaptazodis, Aprasas, Patirties_taskai,Salis, Trumpas_aprasas, Ugis, Svoris, Kalba, Plauku_spalva, Lytis, Akiu_spalva, fk_image__id_image, fk_lygis__id_lygis) 
-                          VALUES ('${newUser.name}', '${newUser.email}', '${newUser.password}', '${newUser.desc}', '0', '${newUser.country}', '${newUser.short_desc}', '${newUser.height}', '${newUser.width}', '${newUser.language}',' ${newUser.hair_col}', '${newUser.gender}', '${newUser.eye_col}', '1', '1')`);
-        res.redirect("/");                          
+        sql.query(`INSERT INTO Naudotojai(Vardas, Elektroninis_pastas, Slaptazodis, Aprasas, Patirties_taskai,Salis, Trumpas_aprasas, Ugis, Svoris, Kalba, Plauku_spalva, Lytis, Akiu_spalva, fk_lygis__id_lygis) 
+                          VALUES ('${newUser.name}', '${newUser.email}', '${newUser.password}', '${newUser.desc}', '0', '${newUser.country}', '${newUser.short_desc}', '${newUser.height}', '${newUser.width}', '${newUser.language}',' ${newUser.hair_col}', '${newUser.gender}', '${newUser.eye_col}','1')`);
+        return res.redirect("/");                         
     } catch(error) {
         console.log(error);
     }
 }
+
 module.exports = {
     getLoginPage: getLoginPage,
     getRegisterPage: getRegisterPage,
-    registerNewUser: registerNewUser
+    registerNewUser: registerNewUser,
+    loginUser: loginUser,
+    getProfilePage: getProfilePage
 }
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
 const {getFavoriteGroup} = require("./control/favoriteController");
-const {getLoginPage, getRegisterPage, registerNewUser} = require("./control/userController");
+const {getLoginPage, getRegisterPage, registerNewUser, loginUser, getProfilePage} = require("./control/userController");
 const {getGamePage} = require("./control/gameController");
 const bodyParser = require("body-parser");
-
+const sessions = require('express-session');
+const cookieParser = require('cookie-parser');
 
 app.use(express.static(__dirname + "/public"));
 //Idiomatic expression in express to route and respond to a client request
@@ -21,11 +22,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/favoriteGroup", getFavoriteGroup);
 
 app.get("/login", getLoginPage);
+app.get("/profile/:id", getProfilePage);
 
 app.get("/game", getGamePage);
 app.get("/register", getRegisterPage);
 
 app.post("/register", registerNewUser);
+app.post("/login", loginUser);
+
+app.use(sessions({
+    secret:"?D(G-KaPdSgVkYp3s6v9y$B&E)H@MbQe",
+    saveUninitialized: true,
+    cookie: { maxAge: 1000*60*60 },
+    resave: false
+}));
+
+app.use(cookieParser());
+
+
 
 app.listen(port, () => {
     console.log(`App listening on port http://localhost:${port}`)
