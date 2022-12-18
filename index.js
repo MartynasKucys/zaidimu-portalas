a6ddf6492f9dfa5305f4522c419b516eea3b9f77
MartynasKucys <martynas.kucys@gmail.com>
2022-12-18 13:55:13 +0200

    added favorite group

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
+
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
+const {getFavoriteGroup} = require("./control/favoriteController");
 const {getLoginPage, getRegisterPage} = require("./control/userController");
 const {getGamePage} = require("./control/gameController");
 
@@ -10,7 +13,14 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
     res.render('index.ejs', {root: __dirname});  
 });
 
+app.get("/favoriteGroup", getFavoriteGroup);
+
 app.get("/login", getLoginPage);
 
 app.get("/game", getGamePage);
 app.get("/register", getRegisterPage);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+  })
