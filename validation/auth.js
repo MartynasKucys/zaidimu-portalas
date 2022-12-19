aea9023dbdad1a77cdd32c443b4a7ee1d520f566
itastylt <100284073+itastylt@users.noreply.github.com>
2022-12-19 02:02:27 +0200

    fix

--- validation/auth.js
+++ validation/auth.js
@@ -3,22 +3,24 @@ const sql = require("../configs/connect.js")
 module.exports = {
     checkRegister:
         function checkRegister(fields , err) {
-            let errors = [];
-            console.log(fields);
-            if(!fields.name || !fields.password || !fields.email || !fields.language || !fields.height || !fields.width || !fields.hair_col || !fields.eye_col || !fields.gender || !fields.short_desc || !fields.desc || !fields.country) {
-                errors.push("Visi laukeliai privalo buti uzpildyti !")
-            }
 
-            sql.query(`Select * FROM Naudotojai Where Vardas='${fields.name}' or Elektroninis_pastas='${fields.email}'`, (err, res) => {
-                if(err) {
+
+            sql.query(`Select * FROM Naudotojai Where Vardas='${fields.name}' or Elektroninis_pastas='${fields.email}'`, (error, res) => {
+                if(error) {
                     return console.log(err);
                 }
-                
+                let errors = [];
                 if(res.length > 0) {
                     errors.push("Toks vartotojas jau egzistuoja!");
                 }
+
+                console.log(fields);
+                if(!fields.name || !fields.password || !fields.email || !fields.language || !fields.height || !fields.width || !fields.hair_col || !fields.eye_col || !fields.gender || !fields.short_desc || !fields.desc || !fields.country) {
+                    errors.push("Visi laukeliai privalo buti uzpildyti !")
+                }
+                return err(errors);
             });
         
-            return err(errors);
+
         }
 }
